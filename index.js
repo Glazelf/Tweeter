8dacd4056468c67205ba79e62480365197fe25c3
Glazelf <gideon.steenbergen@gmail.com>
2021-01-31 20:11:42 +0100

    make code functional

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Twitter = require('twitter-v2');
+const Twitter = require('twitter-lite');
 const config = require('./config.json');
 
 const client = new Twitter({
@@ -7,3 +7,16 @@ const client = new Twitter({
     access_token_key: config.AccessToken,
     access_token_secret: config.AccessTokenSecret,
 });
+
+// Set body to an object
+let postBody = {
+    'status': config.TweetText
+};
+
+client.post('statuses/update', postBody).then(result => {
+    console.log(`You successfully tweeted this: "${result.text}"`);
+}).catch(console.error);
+
+console.log("aa");
+
+return console.log("Success!")
