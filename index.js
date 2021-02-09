450d2ac242800c28cb4bebafd2b49ad01bbe9002
Glazelf <gideon.steenbergen@gmail.com>
2021-02-09 18:03:43 +0100

    make string cleaner

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ let postBody = {
 // Tweet
 client.post('statuses/update', postBody).catch(console.error).then(result => {
     // console.log(result);
-    return console.log(`You successfully tweeted this: "${result.text}"`);
+    return console.log(`You successfully tweeted: "${result.text}"`);
 });
